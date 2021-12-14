66dcfffdd7a7746a0b51e97ef820af2b4e84b51c
lenovo <lenovo@DESKTOP-OD0752Q>
2021-12-14 11:06:24 +0530

    organizations created

--- src/test/java/git_Practicedemo/demo1.java
+++ src/test/java/git_Practicedemo/demo1.java
@@ -1,5 +1,7 @@
 package git_Practicedemo;
 
 public class demo1 {
-
+public static void main(String[] args) {
+	System.out.println("demo new class created");
+}
 }
---
+++ src/test/java/git_Practicedemo/demo4_org.java
@@ -0,0 +1,7 @@
+package git_Practicedemo;
+
+public class demo4_org {
+public static void main(String[] args) {
+	System.out.println("organization branch created succesfully.");
+}
+}
