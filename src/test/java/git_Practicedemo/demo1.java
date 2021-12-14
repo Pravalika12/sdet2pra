1b98c8c5f9e7a240d7479fa776d8509b513e8476
Admin <Admin@DESKTOP-I4J4Q23>
2021-12-14 12:17:55 +0530

    new update

--- src/test/java/git_Practicedemo/demo1.java
+++ src/test/java/git_Practicedemo/demo1.java
@@ -3,5 +3,7 @@ package git_Practicedemo;
 public class demo1 {
 public static void main(String[] args) {
 	System.out.println("demo new class created");
+	
+	System.out.println("Shruthi changes");
 }
 }
